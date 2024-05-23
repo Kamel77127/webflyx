bab9e93f4de3211529b6a23212bf87c923202240
namir77127 <mkamel77127@gmail.com>
2024-05-23 18:44:14 +0200

    on ajoute git.py

---
+++ git.py
@@ -0,0 +1,7 @@
+# git init
+# git status nous dit quel fichier est commited dans le repo ou pas 
+#git add le lien vers le fichier pour l'ajouter à la liste de fichie rà commit
+#git commit -m "messag"
+#git commit --amend -m "message" si je me suis trompé
+# git log pour voir l'historique des commit
+#git log -n --no-pager
